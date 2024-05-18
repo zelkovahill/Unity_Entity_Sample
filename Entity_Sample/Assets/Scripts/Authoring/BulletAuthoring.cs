62b00c67eb66fe20773a2fe9c33e47a89dda07a1
Woomin <changwoomin426@gmail.com>
2024-05-18 19:42:54 +0900

    feat : Authoring 추가

---
+++ Entity_Sample/Assets/Scripts/Authoring/BulletAuthoring.cs
@@ -0,0 +1,19 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+class BulletAuthoring : MonoBehaviour
+{
+    
+}
+
+class BulletAuthoringBaker : Baker<BulletAuthoring>
+{
+    public override void Bake(BulletAuthoring authoring)
+    {
+        var entity = GetEntity(authoring.gameObject, TransformUsageFlags.Dynamic);
+
+        AddComponent(entity, new Bullet());
+        AddComponent(entity, new Movement { Velocity = new float3(0, 0, 1) });
+    }
+}
---
+++ Entity_Sample/Assets/Scripts/Authoring/BulletSpawnDataAuthoring.cs
@@ -0,0 +1,28 @@
+using Unity.Entities;
+using UnityEngine;
+
+class BulletSpawnDataAuthoring : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public int BulletsPerSpawn = 100;
+    public double TimeBetweenSpawns = 0.2f;
+    public float BulletSpeed = 8f;
+}
+
+class BulletSpawnDataAuthoringBaker : Baker<BulletSpawnDataAuthoring>
+{
+    public override void Bake(BulletSpawnDataAuthoring authoring)
+    {
+        var entity = GetEntity(authoring.gameObject, TransformUsageFlags.None);
+
+        var bulletSpawnData = new BulletSpawnData();
+        bulletSpawnData.BulletPrefab = GetEntity(authoring.BulletPrefab, TransformUsageFlags.Dynamic);
+        bulletSpawnData.BulletsPerSpawn = authoring.BulletsPerSpawn;
+        bulletSpawnData.TimeBetweenSpawns = authoring.TimeBetweenSpawns;
+        bulletSpawnData.BulletSpeed = authoring.BulletSpeed;
+        bulletSpawnData.NextSpawnTime = 0;
+
+        AddComponent(entity, bulletSpawnData);
+    }
+    
+}
